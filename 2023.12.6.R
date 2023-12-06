e027fcdff8c8f7dfe1624da6135528acdf23533a
ying <144878810+411273048@users.noreply.github.com>
2023-12-06 13:40:09 +0800

    Add files via upload

---
+++ 2023.12.6.R
@@ -0,0 +1,31 @@
+{
+  x+3
+}
+
+values_x<-c(2,4,7,15)
+seq_along(values_x)
+for(x in seq_along(values_x))
+{
+  values_x[[x]]+3
+}
+
+
+
+
+
+myfun()
+
+
+
+
+# Task by situation
+
+oneComingStudentMajor<-sample(c("Economics"))
+print(oneComingStudentMajor)
+
+# code block1
+if(oneComingStudentMajor == "Economics")
+{
+  #oneComingStudentMajor == "Economics"
+  
+}
